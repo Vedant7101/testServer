fb979ff52194a9a33a1c00a1674acb46939144a5
Vedant7101 <60612162+Vedant7101@users.noreply.github.com>
2021-11-24 19:25:01 +0530

    first

---
+++ testServer.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const enableWs = require('express-ws')
+
+const app = express()
+enableWs(app)
+
+app.ws('/echo', (ws, req) => {
+    ws.on('message', msg => {
+        ws.send(msg)
+    })
+
+    ws.on('close', () => {
+        console.log('WebSocket was closed')
+    })
+})
+
+app.listen(80)
