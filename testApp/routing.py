05f3433104840ca63c79a969a002750c61cfb678
Vedant7101 <60612162+Vedant7101@users.noreply.github.com>
2021-11-27 17:06:59 +0530

    Hello

--- testApp/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from testApp.consumers import *
-
-
-ws_patterns = [
-    path('<name>', Users)
-]
--- testApp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from testApp.views import *
-
-urlpatterns = [
-    path('', home)
-]
